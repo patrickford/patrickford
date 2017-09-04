d91d67a55331547b776255190a37346ffe3e8569
Patrick Ford <patrickford@msn.com>
2017-09-04 07:44:02 -0700

    setup

---
+++ js/patrickford.js
@@ -0,0 +1,40 @@
+function showCopy(i) {
+	$("p").append(topicCopy[i]);
+}
+
+var topicCopy = new Array();
+topicCopy[0] = "Greetings!  This site provides a little information on my background and interests. The next few slides
+               will present some details from my life as an Entrepreneur, Executive, Developer, Comedian, Scuba Instructor, and Poker Player.";
+topicCopy[1] = "I have always been entrepreneurial.  From starting a clamming business at 16 with the savings from my paper route, to
+               started a consulting business in college providing statistical analysis, to founding multiple startups after graduate school.";
+topicCopy[2] = "Over the years I have founded several more companies raising financing
+               from both angel investors and well as venture capital firms.  In 1995 I founded Splash Studios, a game
+               company that produced the interactive video CD-ROM Piper as well as Splash.com The Online Magazine for
+               Kids. In 2000 I founded MrSwap.com, a site for trading movies, music and games, and in 2003 I founded
+               All-Seeing Eye Inc. which took a small shareware product for videogame matchmaking developed by two
+               young programmers in Finland and sold it to Yahoo!  I am currently working on plans for several other 
+               startups in the areas of: online gaming, wearable technology, and live improvisational comedy using
+               motion capture technology and real time 3D rendering.";
+topicCopy[3] = "When not running my own small startups I have spent much of my career as a corporate executive working 
+               for some of the largest companies in the world.  I have sometimes overseen product development activities
+               and other times been in charge of marketing.  I have served in such positions as: Game Studio Vice President 
+               for Fujitsu, Vice President of Marketing (and later VP of Product Development) for GameSpy, Sr. Vice 
+               President of Social and Mobile Games for Atari.";
+topicCopy[4] = "I discovered my passion for software development just 15 minutes into the first lecture of Computer Science
+               101 in college.  I knew right then and there that this is what I wanted to do for a living.  I absorbed
+               as much information as I could and learned to program in all of the current languages, and for all 
+               operating systems.  For about the first decade of my career I developed software and was hired as a
+               Software Design Engineer at Microsoft in the early 90's.  After leaving Microsoft I moved up the ranks
+               into executive management and stopped actively coding.  I am looking forward to bringing my development
+               skills back up to date and once again become an active developer.";
+topicCopy[5] = "I realized one of my lifelong dreams of doing stand up comedy in 2007 when I took an 8-week workshop in 
+               Los Angeles and did my debut show at the world famous Hollywood Improv.  Right away I was hooked and since
+               then have performed hundreds of times at venues large and small, produced my own show called Comediaplex
+               for three years and have appeared on TV in Last Comic Standing, and in more than a dozen online video shorts
+               for sites such as Funny or Die.";
+topicCopy[6] = "I have been an avid scuba diver since 1990, but decided at age 43 to enroll in the training program to become
+               both a rescue diver and certified instructor.";
+topicCopy[7] = "One of my favorite interests is playing poker.  I have been playing since my teens and honed my skills in the 
+               casinos of Lake Tahoe while living there for a winter in 1988.  Over the years I have won many live and online
+               tournaments including fininshing in the money at The World Poker Tour.";
+
